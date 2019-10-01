e42fc8b99aa76e8786dfc99e613c9bf3fd15dd67
OhSungHyeon <osh@robotis.com>
2019-10-01 21:31:43 +0900

    Removed unnecessary text

--- examples_rclcpp/src/action_server/main.cpp
+++ examples_rclcpp/src/action_server/main.cpp
@@ -17,32 +17,20 @@ void print_help()
 
 int main(int argc, char *argv[])
 {
-  // Force flush of the stdout buffer.
-  // This ensures a correct sync of all prints
-  // even when executed simultaneously within the launch file.
   setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 
-  // Find specified argument option
   if (rcutils_cli_option_exist(argv, argv + argc, "-h"))
   {
     print_help();
     return 0;
   }
 
-  // This function initializes any global resources needed by the middleware and the client library,
-  // as well as doing client library related command line argument parsing.
   rclcpp::init(argc, argv);
 
-  // Load node on shared pointer
   auto node = std::make_shared<robotis::Light>();
 
-  // Create a default single-threaded executor and spin the specified node
   rclcpp::spin(node);
 
-  // The shutdown function causes all nodes and
-  // their constituent parts to become invalid and shutdown.
-  // It also destroys any global resources created
-  // when the initialization function was originally called.
   rclcpp::shutdown();
 
   return 0;
