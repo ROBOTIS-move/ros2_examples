a05a97afb9e2033953cf175b03540576df6e9a03
OhSungHyeon <osh@robotis.com>
2019-10-01 19:07:38 +0900

    Removed unnecessary comments

--- examples_rclcpp/src/action_client/main.cpp
+++ examples_rclcpp/src/action_client/main.cpp
@@ -21,23 +21,16 @@ void print_help()
 
 int main(int argc, char *argv[])
 {
-  // Force flush of the stdout buffer.
-  // This ensures a correct sync of all prints
-  // even when executed simultaneously within the launch file.
   setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 
-  // Find specified argument option
   if (rcutils_cli_option_exist(argv, argv + argc, "-h"))
   {
     print_help();
     return 0;
   }
 
-  // This function initializes any global resources needed by the middleware and the client library,
-  // as well as doing client library related command line argument parsing.
   rclcpp::init(argc, argv);
 
-  // Get data from specified argument
   char * cmd_line_interface_option = rcutils_cli_get_option(argv, argv+argc, "-n");
 
   int input_value = 5;
@@ -46,7 +39,6 @@ int main(int argc, char *argv[])
      input_value=atoi(cmd_line_interface_option);
   }
 
-  // Load node on shared pointer
   auto node = std::make_shared<robotis::Switch>(input_value);
 
   while(!node->is_goal_done())
@@ -54,12 +46,7 @@ int main(int argc, char *argv[])
     rclcpp::spin_some(node);
   }
 
-  // The shutdown function causes all nodes and
-  // their constituent parts to become invalid and shutdown.
-  // It also destroys any global resources created
-  // when the initialization function was originally called.
   rclcpp::shutdown();
-
 
   return 0;
 
