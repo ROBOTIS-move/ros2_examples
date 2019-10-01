b8be81e3321ca177c2740fd542b391862c6fe5d2
OhSungHyeon <osh@robotis.com>
2019-10-01 15:37:25 +0900

    Modified vertical whitespace in include

--- examples_rclcpp/include/action_server/light.hpp
+++ examples_rclcpp/include/action_server/light.hpp
@@ -1,27 +1,20 @@
 #ifndef EXAMPLES_RCLCPP_ACTION_SERVER_LIGHT_HPP_
 #define EXAMPLES_RCLCPP_ACTION_SERVER_LIGHT_HPP_
 
-
 #include <memory>
-
 #include <string>
-
 #include <utility>
 
-
 #include <examples_msgs/action/led.hpp>
-
 #include "rclcpp/rclcpp.hpp"
-
 #include "rclcpp_action/rclcpp_action.hpp"
-
 
 
 namespace robotis
 {
     class Light : public rclcpp::Node
     {
-    public:   
+    public:
       // 'explicit' makes block data conversion
       explicit Light();
 
@@ -30,11 +23,11 @@ namespace robotis
       void message_info();
 
       void execute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<examples_msgs::action::Led>> goal_handle);
-      
+
       // Declare action server
       rclcpp_action::Server<examples_msgs::action::Led>::SharedPtr action_server_;
-   
+
     };
-     
+
 }
 #endif // EXAMPLES_RCLCPP_SERVER_CALCULATOR_HPP_
